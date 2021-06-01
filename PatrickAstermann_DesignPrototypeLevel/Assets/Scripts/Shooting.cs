c23d0aff23eefe79b477b26967e566ced5b88515
Patrick <patrick.astermann01@gmail.com>
2021-06-01 16:57:57 +1000

    FEATURE (CONTROLS) (GUI)
    Added:
    -camera movement
    -shooting
    -Points on HUD
    -FPS dot

---
+++ PatrickAstermann_DesignPrototypeLevel/Assets/Scripts/Shooting.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    //VARS
+    public Camera fpsCam;
+    public float range;
+    public KeyCode Key;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(Key))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if(Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
+        {
+            if (hit.transform.gameObject.layer == 9)
+            {
+                hit.transform.SendMessage("HitByRay");
+            }
+            Debug.Log(hit.transform.name);
+        }
+    }
+}
